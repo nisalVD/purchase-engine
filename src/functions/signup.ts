eead6c86aa9592ed918e51ddbc4f960b2544ed1f
Nisal Don <nisalvd@gmail.com>
2022-06-03 19:31:50 +1000

    add signup that generates jwt

--- src/functions/signup.ts
+++ src/functions/signup.ts
@@ -1,9 +1,70 @@
 import { Handler } from "@netlify/functions";
+import jwt from "jsonwebtoken";
+import { z } from "zod";
+import { Customer } from "../discountEngine/types";
 
-const handler:  = async (event, contect) => {
+const handler: Handler = async (event, context) => {
+  if (event.httpMethod === "POST") {
+    try {
+      const body = event.body && JSON.parse(event.body);
+
+      const User = z.object({
+        email: z.string(),
+        name: z.string(),
+        password: z.string(),
+        phoneNumber: z.string(),
+        deliveryAddress: z.string(),
+      });
+
+      const parsed = User.safeParse(body);
+      if (!parsed.success) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify(parsed.error),
+        };
+      }
+
+      const { name, email, phoneNumber, deliveryAddress } = parsed.data;
+
+      const customer: Customer = {
+        name,
+        email,
+        phoneNumber,
+        deliveryAddress,
+      };
+
+      const token = jwt.sign(
+        {
+          exp: Math.floor(Date.now() / 1000) + 60 * 60,
+          ...customer,
+        },
+        process.env.JWT_SECRET as string
+      );
+
+      const response = {
+        token,
+      };
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response),
+      };
+    } catch (err) {
+      if (err instanceof Error) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ error: err.message }),
+        };
+      }
+      return {
+        statusCode: 500,
+        error: err,
+      };
+    }
+  }
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ hello: "world" }),
+    statusCode: 404,
   };
 };
 
